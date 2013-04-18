d831beddfad805bcfc53d95885ea99fc9cc4911e
nadams <nadams@node-3.net>
2013-04-18 07:35:06 -0600

    Adding an interface for socket work

---
+++ Provider/Net/Provider.Net.Sockets/ISocketApi.cs
@@ -0,0 +1,5 @@
+﻿namespace Zander.Provider.Net.Sockets {
+	public interface ISocketApi {
+
+	}
+}
--- Provider/Net/Provider.Net.Sockets/ZandronumMasterServerRepository.cs
+++ Provider/Net/Provider.Net.Sockets/ZandronumMasterServerRepository.cs
@@ -3,6 +3,12 @@ using Zander.Domain.Entities;
 
 namespace Zander.Provider.Net.Sockets {
 	public class ZandronumMasterServerRepository : IMasterServerRepository {
+		private readonly ISocketApi sockets;
+
+		public ZandronumMasterServerRepository(ISocketApi socketApi) {
+			this.sockets = socketApi;
+		}
+
 		public IMasterServer Get(string address) {
 			throw new System.NotImplementedException();
 		}
