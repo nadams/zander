ab615aa54375579c599a6d76cca1debbceb969ef
nadams <nadams@node-3.net>
2013-04-18 07:11:07 -0600

    Defining CompatFlags from Zandronum source

--- Zander.Domain/Entities/CompatFlags.cs
+++ Zander.Domain/Entities/CompatFlags.cs
@@ -4,6 +4,36 @@ namespace Zander.Domain.Entities {
 
 	[Flags]
 	public enum CompatFlags : uint {
-
+		UseShortestTexture = 1 << 0,
+		UseOldStairIndex = 1 << 1,
+		LimitPainElemental = 1 << 2,
+		SilentPickups = 1 << 3,
+		InfinitelyTallActors = 1 << 4,
+		MagicSilence = 1 << 5,
+		Wallrun = 1 << 6,
+		SpawnItemsOnFloow = 1 << 7,
+		UseBlocking = 1 << 8,
+		NoDoorLight = 1 << 9,
+		UseRavenScroll = 1 << 10,
+		OldSoundCode = 1 << 11,
+		LimitDehHelth = 1 << 12,
+		Trace = 1 << 13,
+		Dropoff = 1 << 14,
+		BoomScroll = 1 << 15,
+		Invisibility = 1 << 16,
+		SilentInstantFloows = 1 << 27,
+		UseOldSectorSoundOrigin = 1 << 28,
+		UseOldMissileClip = 1 << 29,
+		MonsterDropoff = 1 << 30,
+		LimitedAirMovement = 1 << 17,
+		PlasmaBumpBug = 1 << 18,
+		AllowInstantRespawn = 1 << 19,
+		DisableTaunts = 1 << 20,
+		UseOriginalSoundCurve = 1 << 21,
+		UseOldIntermission = 1 << 22,
+		DisableStealthMonsters = 1 << 23,
+		UseOldRadiusDamage = 1 << 24,
+		NoCrosshair = 1 << 25,
+		OldWeaponSwitch = 1 << 26
 	}
 }
