a636619921c428ab4df88c65cecb94e213c53480
nadams <nadams@node-3.net>
2013-04-18 16:56:27 -0600

    Adding blank test

---
+++ Zander.UnitTests/Provider/Net/Sockets/ZandronumMasterServerRepositoryTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zander.UnitTests.Provider.Net.Sockets {
+
+	[TestClass]
+	public class ZandronumMasterServerRepositoryTests {
+
+		[TestMethod]
+		public void TestMethod1() {
+
+		}
+	}
+}
