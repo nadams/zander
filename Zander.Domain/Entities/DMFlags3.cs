56617ac90aa7d9b4c705f337748dc9a7af850a5c
nadams <nadams@node-3.net>
2013-04-18 07:03:59 -0600

    Defining DMFlags3 from Zandronum source

--- Zander.Domain/Entities/DMFlags3.cs
+++ Zander.Domain/Entities/DMFlags3.cs
@@ -4,6 +4,10 @@ namespace Zander.Domain.Entities {
 
 	[Flags]
 	public enum DMFlags3 : uint {
-
+		NoTargetIdentify = 1 << 0,
+		ApplyLmsSpectatorSettings = 1 << 1,
+		NoCoopInfo = 1 << 2,
+		NoUnlagged = 1 << 3,
+		UnblockPlayers = 1 << 4
 	}
 }
