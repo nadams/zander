6a12e2dbbc0641fe7921714a0316bf39ba1e31d4
nadams <nadams@node-3.net>
2013-04-18 07:13:17 -0600

    Defining CompatFlags2 from Zandronum source

--- Zander.Domain/Entities/CompatFlags2.cs
+++ Zander.Domain/Entities/CompatFlags2.cs
@@ -4,6 +4,14 @@ namespace Zander.Domain.Entities {
 
 	[Flags]
 	public enum CompatFlags2 : uint {
-
+		NetScriptsAreClientSide = 1 << 0,
+		ClientSendsFullButtonInfo = 1 << 1,
+		NoLand = 1 << 2,
+		UseOldNumberGenerator = 1 << 3,
+		NoGravitySpheres = 1 << 4,
+		DoNotStopPlayerScriptsOnDisconnect = 1 << 5,
+		OldExplosionThrust = 1 << 6,
+		OldBridgeDrops = 1 << 7,
+		UseOldZDoomJumpPhysics = 1 << 8
 	}
 }
