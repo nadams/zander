a0249fc8e5b7be94c829882f39cdf85685c5d9c9
nadams <nadams@node-3.net>
2013-04-18 07:21:39 -0600

    Definint MasterServerRepository

---
+++ Provider/Net/Provider.Net.Sockets/ZandronumMasterServerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Zander.Domain;
+using Zander.Domain.Entities;
+
+namespace Zander.Provider.Net.Sockets {
+	public class ZandronumMasterServerRepository : IMasterServerRepository {
+		public IMasterServer Get(string address) {
+			throw new System.NotImplementedException();
+		}
+	}
+}
