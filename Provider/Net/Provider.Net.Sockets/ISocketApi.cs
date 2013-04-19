5fb015445e26bcb791e7b72958872f88b15bdef3
nadams <nadams@node-3.net>
2013-04-18 18:22:00 -0600

    Altering ISocketApi

--- Provider/Net/Provider.Net.Sockets/ISocketApi.cs
+++ Provider/Net/Provider.Net.Sockets/ISocketApi.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
+
 namespace Zander.Provider.Net.Sockets {
 	public interface ISocketApi {
-		Stream Get(string address);
+		Stream GetWriteStream(string address);
+		Stream GetReadStream(Stream s);
 	}
 }
