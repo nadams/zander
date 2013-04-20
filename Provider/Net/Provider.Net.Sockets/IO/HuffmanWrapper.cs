b5ea66cc4757f62c6d3a88f0c25ce3ff29b2ad55
nadams <nadams@node-3.net>
2013-04-20 08:34:04 -0600

    Moving Huffman into a dll. Makign a wrapper around the static huffman method calls

---
+++ Provider/Net/Provider.Net.Sockets/IO/HuffmanWrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace Zander.Provider.Net.Sockets.IO {
+	public class HuffmanWrapper : IHuffmanEncoding {
+
+		public byte[] Encode(byte[] decodedData) {
+			return Huffman.Encode(decodedData);
+		}
+
+		public byte[] Decode(byte[] encodedData) {
+			return Huffman.Decode(encodedData);
+		}
+	}
+}
---
+++ Provider/Net/Provider.Net.Sockets/IO/IHuffmanEncoding.cs
@@ -0,0 +1,6 @@
+﻿namespace Zander.Provider.Net.Sockets.IO {
+	public interface IHuffmanEncoding {
+		byte[] Encode(byte[] decodedData);
+		byte[] Decode(byte[] encodedData);
+	}
+}
