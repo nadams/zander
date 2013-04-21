041f65f507448da5d6981631750fb4650200976d
nadams <nadams@node-3.net>
2013-04-21 08:03:34 -0600

    Creating a ChallengeValues enum for easy access to the protocol's values

---
+++ Zander.UnitTests/Provider/Net/Sockets/HuffmanTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zander.UnitTests.Provider.Net.Sockets {
+
+	[TestClass]
+	public class HuffmanTests {
+		
+		[TestMethod]
+		public void Encode_() {
+
+		}
+
+		[TestMethod]
+		public void Decode_() {
+
+		}
+	}
+}
