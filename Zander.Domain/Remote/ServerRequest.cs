a9ad8b2004bf4387a2aa97fd546db3c35b0f87b8
nadams <nadams@node-3.net>
2013-04-21 15:36:33 -0600

    Filling in ServerRequest

--- Zander.Domain/Remote/ServerRequest.cs
+++ Zander.Domain/Remote/ServerRequest.cs
@@ -1,9 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net;
 
 namespace Zander.Domain.Remote {
 	public class ServerRequest {
+		private readonly IPEndPoint address;
+		private readonly uint query;
+		private readonly int challenge;
+
+		public IPEndPoint Address {
+			get { return this.address; }
+		}
+
+		public uint Query {
+			get { return this.query; }
+		}
+
+		public int Challenge {
+			get { return this.challenge; }
+		}
+
+		public ServerRequest(IPEndPoint address, uint query, int challenge) {
+			this.address = address;
+			this.query = query;
+			this.challenge = challenge;
+		}
 	}
 }
