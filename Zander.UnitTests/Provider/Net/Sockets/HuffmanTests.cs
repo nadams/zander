3b333a87b5a3c05acbb77a5579a3f9e25d2abfa0
nadams <nadams@node-3.net>
2013-04-21 12:04:52 -0600

    Writing a couple tests for huffman

--- Zander.UnitTests/Provider/Net/Sockets/HuffmanTests.cs
+++ Zander.UnitTests/Provider/Net/Sockets/HuffmanTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zander.Provider.Net.Sockets.IO;
 
 namespace Zander.UnitTests.Provider.Net.Sockets {
 
@@ -6,13 +8,32 @@ namespace Zander.UnitTests.Provider.Net.Sockets {
 	public class HuffmanTests {
 		
 		[TestMethod]
-		public void Encode_() {
-			Assert.Fail();
+		public void Encode_EncodeData_MatchesPreEncodedData() {
+			var preEncodedData = new byte[] { 255, 124, 93, 86, 0, 2, 0 };
+
+			var dataToEncode = new byte[] { 124, 93, 86, 0, 2, 0 };
+
+			var huffmanWrapper = new HuffmanWrapper();
+			var encodedData = new byte[1024];
+
+			var encodedLength = huffmanWrapper.Encode(dataToEncode, encodedData, dataToEncode.Length);
+			encodedData = encodedData.Take(encodedLength).ToArray();
+
+			Assert.IsTrue(Enumerable.SequenceEqual(preEncodedData, encodedData));
 		}
 
 		[TestMethod]
-		public void Decode_() {
-			Assert.Fail();
+		public void Decode_DecodeData_MatchesPreDecodedData() {
+			var rawData = new byte[] { 124, 93, 86, 0, 2, 0 };
+
+			var huffmanWrapper = new HuffmanWrapper();
+			var decodedData = new byte[1024];
+
+			var preEncodedData = new byte[] { 255, 124, 93, 86, 0, 2, 0 };
+			var decodedLength = huffmanWrapper.Decode(preEncodedData, decodedData, preEncodedData.Length);
+			decodedData = decodedData.Take(decodedLength).ToArray();
+
+			Assert.IsTrue(Enumerable.SequenceEqual(rawData, decodedData));
 		}
 	}
 }
