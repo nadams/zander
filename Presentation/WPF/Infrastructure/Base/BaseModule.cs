0bad11caee22fb95cf01f143f0ed8555d8d7b3f6
nadams <nadams@node-3.net>
2013-04-25 19:55:16 -0600

    Adding module for MenuBar

---
+++ Presentation/WPF/Infrastructure/Base/BaseModule.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+
+namespace Zander.Presentation.WPF.Zander.Infrastructure.Base {
+	public abstract class BaseModule : IModule {
+		protected readonly IUnityContainer container;
+		protected readonly IRegionManager regionManager;
+
+		public BaseModule(IUnityContainer container, IRegionManager regionManager) {
+			this.container = container;
+			this.regionManager = regionManager;
+		}
+
+		public abstract void Initialize();
+	}
+}
---
+++ Presentation/WPF/Modules/MenuBar/MenuBarModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using Zander.Presentation.WPF.Zander.Infrastructure.Base;
+
+namespace MenuBar {
+	public class MenuBarModule : BaseModule {
+		public MenuBarModule(IUnityContainer container, IRegionManager regionManager)  : base(container, regionManager) { }
+
+		public override void Initialize() {
+
+		}
+	}
+}
