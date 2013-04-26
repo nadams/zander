092801570d35d4c085008dd21ea20d59be0369f2
nadams <nadams@node-3.net>
2013-04-25 20:04:12 -0600

    Creating IView and IViewModel

--- Presentation/WPF/Modules/MenuBar/MenuBarModule.cs
+++ Presentation/WPF/Modules/MenuBar/MenuBarModule.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Microsoft.Practices.Prism.Regions;
+﻿using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.Unity;
 using Zander.Presentation.WPF.Zander.Infrastructure.Base;
 
 namespace MenuBar {
 	public class MenuBarModule : BaseModule {
-		public MenuBarModule(IUnityContainer container, IRegionManager regionManager)  : base(container, regionManager) { }
+		public MenuBarModule(IUnityContainer container, IRegionManager regionManager) : base(container, regionManager) { }
 
 		public override void Initialize() {
 
