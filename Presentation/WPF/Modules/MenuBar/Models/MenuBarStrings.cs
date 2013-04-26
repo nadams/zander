c9c7ea1f3b2961f7940f91f5e0b2e97896fbe107
nadams <nadams@node-3.net>
2013-04-26 07:34:18 -0600

    Making general menu categories

--- Presentation/WPF/Modules/MenuBar/Models/MenuBarStrings.cs
+++ Presentation/WPF/Modules/MenuBar/Models/MenuBarStrings.cs
@@ -27,9 +27,61 @@ namespace Zander.Modules.MenuBar.Models {
 			}
 		}
 
+		private string edit;
+		public string Edit {
+			get {
+				return this.edit;
+			}
+
+			set {
+				this.edit = value;
+				this.RaisePropertyChanged(() => this.Edit);
+			}
+		}
+
+		private string server;
+		public string Server {
+			get {
+				return this.server;
+			}
+
+			set {
+				this.server = value;
+				this.RaisePropertyChanged(() => this.Server);
+			}
+		}
+
+		private string filter;
+		public string Filter {
+			get {
+				return this.filter;
+			}
+
+			set {
+				this.filter = value;
+				this.RaisePropertyChanged(() => this.Filter);
+			}
+		}
+
+		private string help;
+		public string Help {
+			get {
+				return this.help;
+			}
+
+			set {
+				this.help = value;
+				this.RaisePropertyChanged(() => this.Help);
+			}
+		}
+
 		public MenuBarStrings() {
 			this.File = "File";
 			this.Quit = "Quit";
+			this.Edit = "Edit";
+			this.Server = "Server";
+			this.Filter = "Filter";
+			this.Help = "Help";
 		}
 	}
 }
