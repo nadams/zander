d4694637414fe2fcbe644d961c2fd2748c992a0f
nadams <nadams@node-3.net>
2013-04-25 19:50:33 -0600

    Adding ModuleCatalog extensions

---
+++ Presentation/WPF/Zander/Extensions/ModuleCatalogExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace Zander.Presentation.WPF.Zander.Extensions {
+	public static class ModuleCatalogExtensions {
+		public static void RegisterModule<T>(this IModuleCatalog catalog) where T : IModule {
+			RegisterModule<T>(catalog, InitializationMode.WhenAvailable);
+		}
+
+		public static void RegisterModule<T>(this IModuleCatalog catalog, InitializationMode mode) where T : IModule {
+			var type = typeof(T);
+
+			var module = new ModuleInfo {
+				ModuleName = type.Name,
+				ModuleType = type.AssemblyQualifiedName,
+				InitializationMode = mode
+			};
+
+			catalog.AddModule(module);
+
+		}
+	}
+}
