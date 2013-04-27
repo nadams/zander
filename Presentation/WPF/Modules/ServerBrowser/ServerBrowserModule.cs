df5ba3587babf103c4e6fc65e2af753a59c8a764
nadams <nadams@node-3.net>
2013-04-27 13:14:56 -0600

    Registering dependencies with unity

--- Presentation/WPF/Modules/ServerBrowser/ServerBrowserModule.cs
+++ Presentation/WPF/Modules/ServerBrowser/ServerBrowserModule.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Microsoft.Practices.Prism.Regions;
+﻿using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.Unity;
+using Zander.Presentation.WPF.Zander.Infrastructure;
 using Zander.Presentation.WPF.Zander.Infrastructure.Base;
 
 namespace Zander.Modules.ServerBrowser {
@@ -9,7 +9,10 @@ namespace Zander.Modules.ServerBrowser {
 		public ServerBrowserModule(IUnityContainer container, IRegionManager regionManager) : base(container, regionManager) { }
 
 		public override void Initialize() {
+			this.container.RegisterType<IServerBrowserViewModel, ServerBrowserViewModel>();
+			this.container.RegisterType<IServerBrowserView, ServerBrowserControl>();
 
+			this.regionManager.RegisterViewWithRegion(Regions.ServerBrowser, () => this.container.Resolve<IServerBrowserView>());
 		}
 	}
 }
