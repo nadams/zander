bfa33151d475ce4c09c4e6654500ea9b3586abf2
nadams <nadams@node-3.net>
2013-05-12 17:06:13 -0600

    Adding statusbar module

---
+++ Presentation/WPF/Modules/StatusBar/StatusBarModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using Zander.Presentation.WPF.Zander.Infrastructure.Base;
+
+namespace Zander.Modules.StatusBar {
+	public class StatusBarModule : BaseModule {
+
+		public StatusBarModule(IUnityContainer container, IRegionManager regionManager) : base(container, regionManager) { }
+
+		public override void Initialize() {
+			throw new NotImplementedException();
+		}
+	}
+}
