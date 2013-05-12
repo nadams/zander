789a4c2c52efbd19729f3762a19bf8be215d7bbf
nadams <nadams@node-3.net>
2013-05-12 17:14:22 -0600

    Creating ServerBarModel

---
+++ Presentation/WPF/Modules/StatusBar/Models/StatusBarModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.ViewModel;
+
+namespace Zander.Modules.StatusBar.Models {
+	public class StatusBarModel {
+
+	}
+}
--- Presentation/WPF/Modules/StatusBar/StatusBarViewModel.cs
+++ Presentation/WPF/Modules/StatusBar/StatusBarViewModel.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.Practices.Prism.Events;
 using Microsoft.Practices.Prism.ViewModel;
+using Zander.Modules.StatusBar.Models;
 using Zander.Presentation.WPF.Zander.Infrastructure.Events;
 
 namespace Zander.Modules.StatusBar {
 	public class StatusBarViewModel : NotificationObject, IStatusBarViewModel {
 		private readonly IEventAggregator eventAggregator;
 
+		private StatusBarModel statusBarModel;
+		public StatusBarModel StatusBarModel {
+			get {
+				return this.statusBarModel;
+			}
+
+			set {
+				this.statusBarModel = value;
+				this.RaisePropertyChanged(() => this.StatusBarModel);
+			}
+		}
+
 		public StatusBarViewModel(IEventAggregator eventAggregator) {
 			this.eventAggregator = eventAggregator;
+
+			this.StatusBarModel = new StatusBarModel();
 
 			this.eventAggregator.GetEvent<TotalServersUpdatedEvent>().Subscribe(count => { });
 		}
