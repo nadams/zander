6ef6c4fb021e4f68cb3c221e7bb1f9dae220a01b
nadams <nadams@node-3.net>
2013-05-12 17:23:49 -0600

    Registering dependencies in StatusBarModule

--- Presentation/WPF/Modules/StatusBar/StatusBarModule.cs
+++ Presentation/WPF/Modules/StatusBar/StatusBarModule.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Microsoft.Practices.Prism.Regions;
+﻿using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.Unity;
+using Zander.Presentation.WPF.Zander.Infrastructure;
 using Zander.Presentation.WPF.Zander.Infrastructure.Base;
 
 namespace Zander.Modules.StatusBar {
@@ -9,7 +9,10 @@ namespace Zander.Modules.StatusBar {
 		public StatusBarModule(IUnityContainer container, IRegionManager regionManager) : base(container, regionManager) { }
 
 		public override void Initialize() {
-			throw new NotImplementedException();
+			this.container.RegisterType<IStatusBarViewModel, StatusBarViewModel>();
+			this.container.RegisterType<IStatusBarView, StatusBarControl>();
+
+			this.regionManager.RegisterViewWithRegion(Regions.StatusBar, () => this.container.Resolve<IStatusBarView>());
 		}
 	}
 }
