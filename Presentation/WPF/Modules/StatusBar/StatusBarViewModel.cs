542b281ee0ffffc418e650e6a0323f5a3db55e6d
nadams <nadams@node-3.net>
2013-05-12 17:11:59 -0600

    Subscribing to TotalServersUpdatedEvent

--- Presentation/WPF/Modules/StatusBar/StatusBarViewModel.cs
+++ Presentation/WPF/Modules/StatusBar/StatusBarViewModel.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Practices.Prism.Events;
+using Microsoft.Practices.Prism.ViewModel;
+using Zander.Presentation.WPF.Zander.Infrastructure.Events;
 
 namespace Zander.Modules.StatusBar {
-	public class StatusBarViewModel : IStatusBarViewModel {
+	public class StatusBarViewModel : NotificationObject, IStatusBarViewModel {
+		private readonly IEventAggregator eventAggregator;
 
+		public StatusBarViewModel(IEventAggregator eventAggregator) {
+			this.eventAggregator = eventAggregator;
+
+			this.eventAggregator.GetEvent<TotalServersUpdatedEvent>().Subscribe(count => { });
+		}
 	}
 }
