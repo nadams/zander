3fcb1f9720c5f7139317a702b1344e431ac7d65f
nadams <nadams@node-3.net>
2013-05-14 08:08:41 -0600

    Moving items into interface

--- Presentation/WPF/Services/ServerBrowser/IServerBrowserService.cs
+++ Presentation/WPF/Services/ServerBrowser/IServerBrowserService.cs
@@ -1,8 +1,14 @@
-﻿using Zander.Domain.Entities;
+﻿using System.Collections.Specialized;
+using Zander.Domain.Entities;
 
 namespace Zander.Presentation.WPF.Zander.Services.ServerBrowser {
     public interface IServerBrowserService {
+        event NotifyCollectionChangedEventHandler CollectionChanged;
+        event TotalServersUpdatedEventHandler TotalServersUpdated;
+        event DoneQueryingServersEventHandler DoneQueryingServers;
+
         void QueryAllServers();
         void RefreshServer(Server server);
+        void AddServer(Server server);
     }
 }
