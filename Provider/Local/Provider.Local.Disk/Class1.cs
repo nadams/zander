8e7aaab83ff72cafee9c1f017f9eada01d5e9ee8
nadams <nadams@node-3.net>
2014-01-02 08:43:52 -0700

    Moving json reference from domain to disk project

--- Provider/Local/Provider.Local.Disk/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Provider.Local.Disk
-{
-    public class Class1
-    {
-    }
-}
---
+++ Provider/Local/Provider.Local.Disk/ZanderConfigRepository.cs
@@ -0,0 +1,9 @@
+﻿using Zander.Domain.Config;
+
+namespace Provider.Local.Disk {
+    public class ZanderConfigRepository : IZanderConfigRepository {
+        public ZanderConfig GetDefaultConfig() {
+            return null;
+        }
+    }
+}
