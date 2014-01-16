9e7c1b271c5b6a83e6a57c55583aa0d9b3f85a36
nadams <nadams@node-3.net>
2014-01-16 11:43:47 -0700

    Adding confirm and cancel buttons to the settings window

--- Presentation/WPF/Modules/Settings/ISettingsViewModel.cs
+++ Presentation/WPF/Modules/Settings/ISettingsViewModel.cs
@@ -1,5 +1,11 @@
-﻿using Zander.Presentation.WPF.Zander.Infrastructure.Base;
+﻿using System.Collections.Generic;
+using Microsoft.Practices.Prism.Commands;
+using Zander.Presentation.WPF.Zander.Infrastructure.Base;
 
 namespace Zander.Modules.Settings {
-    public interface ISettingsViewModel : IViewModel { }
+    public interface ISettingsViewModel : IViewModel {
+        IEnumerable<ISettingView> Views { get; set; }
+        DelegateCommand OkCommand { get; }
+        DelegateCommand CancelCommand { get; }
+    }
 }
--- Presentation/WPF/Modules/Settings/SettingsViewModel.cs
+++ Presentation/WPF/Modules/Settings/SettingsViewModel.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Windows;
+using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Unity;
 
 namespace Zander.Modules.Settings {
     public class SettingsViewModel : ISettingsViewModel {
         public IEnumerable<ISettingView> Views { get; set; }
+
+        public DelegateCommand OkCommand {
+            get { return new DelegateCommand(() => MessageBox.Show("Ok Clicked")); }
+        }
+
+        public DelegateCommand CancelCommand {
+            get { return new DelegateCommand(() => MessageBox.Show("Cancel Clicked")); }
+        }
 
         public SettingsViewModel(IUnityContainer container) {
             this.Views = new List<ISettingView>();
