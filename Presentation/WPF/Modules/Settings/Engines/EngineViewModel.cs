d0b34e6a8fd9e58bfdf632b999ed6dffcf029f6a
Nick Adams <nadams@node-3.net>
2014-02-04 19:52:54 -0700

    Creating a base view model for the engine view models

---
+++ Presentation/WPF/Modules/Settings/Engines/EngineViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.Practices.Prism.Commands;
+using Zander.Presentation.WPF.Zander.Infrastructure.Base;
+
+namespace Zander.Modules.Settings.Engines {
+    public class EngineViewModel : BaseViewModel {
+        protected readonly ZanderConfigProvider configProvider;
+        protected readonly IFileBrowserProvider fileBrowser;
+
+        public string MasterAddress {
+            get {
+                return this.configProvider.Config.ZandronumConfig.MasterAddress;
+            }
+
+            set {
+                this.configProvider.Config.ZandronumConfig.MasterAddress = value;
+                this.RaisePropertyChanged(() => this.MasterAddress);
+            }
+        }
+
+        public string PathToClinetBinary {
+            get {
+                return this.configProvider.Config.ZandronumConfig.PathToClinetBinary;
+            }
+
+            set {
+                this.configProvider.Config.ZandronumConfig.PathToClinetBinary = value;
+                this.RaisePropertyChanged(() => this.PathToClinetBinary);
+            }
+        }
+
+        public string PathToServerBinary {
+            get {
+                return this.configProvider.Config.ZandronumConfig.PathToServerBinary;
+            }
+
+            set {
+                this.configProvider.Config.ZandronumConfig.PathToServerBinary = value;
+                this.RaisePropertyChanged(() => this.PathToServerBinary);
+            }
+        }
+
+        public string CustomParameters {
+            get {
+                return this.configProvider.Config.ZandronumConfig.CustomParameters;
+            }
+
+            set {
+                this.configProvider.Config.ZandronumConfig.CustomParameters = value;
+                this.RaisePropertyChanged(() => this.CustomParameters);
+            }
+        }
+
+        public DelegateCommand BrowseForClientBinary {
+            get {
+                return new DelegateCommand(() => this.SetBinary(x => this.PathToClinetBinary = x));
+            }
+        }
+
+        public DelegateCommand BrowseForServerBinary {
+            get {
+                return new DelegateCommand(() => this.SetBinary(x => this.PathToServerBinary = x));
+            }
+        }
+
+        public EngineViewModel(ZanderConfigProvider configProvider, IFileBrowserProvider fileBrowser) {
+            this.configProvider = configProvider;
+            this.fileBrowser = fileBrowser;
+        }
+
+        private void SetBinary(Action<string> setAction) {
+            string binary = this.fileBrowser.BrowseForFile();
+
+            if(binary != null) {
+                setAction(binary);
+            }
+        }
+    }
+}
---
+++ Zander.UnitTests/WPF/Modules/Settings/EngineViewModelTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Zander.Domain.Config;
+using Zander.Modules.Settings;
+using Zander.Modules.Settings.Engines;
+
+namespace Zander.UnitTests.WPF.Modules.Settings {
+
+    [TestClass]
+    public class EngineViewModelTests {
+
+        [TestMethod]
+        public void BrowseForClientBinary_FileSelected_ValueSet() {
+            string expected = "client binary";
+
+            var mockFileBrowser = new Mock<IFileBrowserProvider>();
+            mockFileBrowser.Setup(x => x.BrowseForFile()).Returns(expected);
+
+            var settingsProvider = new ZanderConfigProvider { 
+                Config = this.GetNewConfig() 
+            };
+
+            var vm = new EngineViewModel(settingsProvider, mockFileBrowser.Object);
+            vm.BrowseForClientBinary.Execute();
+
+            Assert.AreEqual(expected, vm.PathToClinetBinary);
+        }
+
+        [TestMethod]
+        public void BrowseForClientBinary_FileNotSelected_ValueNotSet() {
+            string expected = null;
+
+            var mockFileBrowser = new Mock<IFileBrowserProvider>();
+            mockFileBrowser.Setup(x => x.BrowseForFile()).Returns(expected);
+
+            var settingsProvider = new ZanderConfigProvider {
+                Config = this.GetNewConfig()
+            };
+
+            var vm = new EngineViewModel(settingsProvider, mockFileBrowser.Object);
+            vm.BrowseForClientBinary.Execute();
+
+            Assert.AreEqual(expected, vm.PathToClinetBinary);
+        }
+
+        [TestMethod]
+        public void BrowseForServerBinary_FileSelected_ValueSet() {
+            string expected = "server binary";
+
+            var mockFileBrowser = new Mock<IFileBrowserProvider>();
+            mockFileBrowser.Setup(x => x.BrowseForFile()).Returns(expected);
+
+            var settingsProvider = new ZanderConfigProvider {
+                Config = this.GetNewConfig()
+            };
+
+            var vm = new EngineViewModel(settingsProvider, mockFileBrowser.Object);
+            vm.BrowseForServerBinary.Execute();
+
+            Assert.AreEqual(expected, vm.PathToServerBinary);
+        }
+
+        [TestMethod]
+        public void BrowseForServerBinary_FileNotSelected_ValueNotSet() {
+            string expected = null;
+
+            var mockFileBrowser = new Mock<IFileBrowserProvider>();
+            mockFileBrowser.Setup(x => x.BrowseForFile()).Returns(expected);
+
+            var settingsProvider = new ZanderConfigProvider {
+                Config = this.GetNewConfig()
+            };
+
+            var vm = new EngineViewModel(settingsProvider, mockFileBrowser.Object);
+            vm.BrowseForServerBinary.Execute();
+
+            Assert.AreEqual(expected, vm.PathToServerBinary);
+        }
+
+        private ZanderConfig GetNewConfig() {
+            return new ZanderConfig();
+        }
+    }
+}
