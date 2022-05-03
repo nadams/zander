6b3d7413f850214439d357029f99c56925751859
Nick Adams <nadams@node-3.net>
2022-05-03 13:27:47 -0600

    Add cli for configuration instead of hard coding values

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,40 +5,77 @@ import (
 	"log"
 	"net"
 	"os"
+	"path/filepath"
+	"reflect"
 
+	"github.com/alecthomas/kong"
+	"github.com/pkg/errors"
 	"gitlab.node-3.net/nadams/zander/zandronum"
 )
 
-const socketAddr = "/tmp/zander.sock"
+type CLI struct {
+	Socket          string `flag:"" short:"s" type:"pathenv" default:"$XDG_CONFIG_HOME/zander/zander.sock" description:"Listen on a socket at the given path"`
+	ZandronumServer string `flag:"" short:"p" type:"pathenv" default:"zandronum-server" description:"The path to zandronum-server, may be just the name if it's in $PATH"`
+}
 
-func main() {
-	//sigs := make(chan os.Signal, 1)
+func pathEnvDecoder() kong.MapperFunc {
+	return func(ctx *kong.DecodeContext, target reflect.Value) error {
+		t, err := ctx.Scan.PopValue("pathenv")
+		if err != nil {
+			return err
+		}
 
-	//signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+		switch v := t.Value.(type) {
+		case string:
+			target.SetString(os.ExpandEnv(v))
+		default:
+			return errors.Errorf("expected a string but got %q (%T)", t, t.Value)
+		}
 
-	//<-sigs
-	//log.Println("shutting down")
-
-	//log.Println(server.Stop())
-	if err := ListenAndServe(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return nil
 	}
 }
 
-func ListenAndServe() error {
-	if err := os.RemoveAll(socketAddr); err != nil {
-		return fmt.Errorf("could not remove zander socket: %w", err)
+func main() {
+	var cli CLI
+	ctx := kong.Parse(&cli, kong.NamedMapper("pathenv", pathEnvDecoder()))
+
+	switch ctx.Command() {
+	default:
+		if err := ListenAndServe(cli); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
+
+func ListenAndServe(cli CLI) error {
+	removeSocket := func() error {
+		if err := os.RemoveAll(cli.Socket); err != nil {
+			return fmt.Errorf("could not remove zander socket: %w", err)
+		}
+
+		return nil
 	}
 
-	l, err := net.Listen("unix", socketAddr)
+	if err := removeSocket(); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(filepath.Dir(cli.Socket), 0755); err != nil {
+		return fmt.Errorf("could not create path for socket: %w", err)
+	}
+
+	l, err := net.Listen("unix", cli.Socket)
 	if err != nil {
 		return fmt.Errorf("could not create socket: %w", err)
 	}
 
 	defer l.Close()
 
-	server := zandronum.NewServer("/usr/bin/zandronum-server", nil)
+	defer removeSocket()
+
+	server := zandronum.NewServer(cli.ZandronumServer, nil)
 	if err := server.Start(); err != nil {
 		log.Println(err)
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module gitlab.node-3.net/nadams/zander
 
 go 1.18
+
+require github.com/alecthomas/kong v0.5.0
+
+require github.com/pkg/errors v0.9.1 // indirect
