b91c3e133faf35e7ee06aa4451d1bffbc59a1fc2
Nick Adams <nadams@node-3.net>
2022-05-18 13:23:58 -0600

    Add some help messages

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 )
 
 type CLI struct {
-	Server command.Server    `cmd:""`
-	Attach command.AttachCmd `cmd:""`
-	List   command.ListCmd   `cmd:""`
+	Server command.Server    `cmd:"" help:"Start zander in server mode"`
+	Attach command.AttachCmd `cmd:"" help:"Attach to a running doom server"`
+	List   command.ListCmd   `cmd:"" help:"List configured doom servers"`
 
 	Socket string `flag:"" short:"s" type:"pathenv" default:"$XDG_CONFIG_HOME/zander/zander.sock" description:"Connects to a socket at the given path"`
 }
