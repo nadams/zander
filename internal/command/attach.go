fff3836160d0936480ac02fe63852528a6e55077
Nick Adams <nadams@node-3.net>
2022-05-25 09:15:09 -0600

    Fix help text

--- internal/command/attach.go
+++ internal/command/attach.go
@@ -21,7 +21,7 @@ import (
 type AttachCmd struct {
 	ID string `arg:"" required:"true" help:"ID of doom server to attach to"`
 
-	Output      string `flag:"" enum:"raw,default" default:"default" help:"Output mode. valid values: (valid values: ${enum})"`
+	Output      string `flag:"" enum:"raw,default" default:"default" help:"Output mode. (valid values: ${enum})"`
 	ScrollLines int    `flag:"" default:"5" help:"How many lines to scroll when pgup and pgdn are pressed. Only valid in default output mode"`
 }
 
