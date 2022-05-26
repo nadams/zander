a47c53993079d22ef184462bf6dcef9e2f0437c3
Nick Adams <nadams@node-3.net>
2022-05-26 09:58:21 -0600

    Use xdg library to find xdg runtime dir

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"github.com/adrg/xdg"
 	log "github.com/sirupsen/logrus"
 
 	"gitlab.node-3.net/nadams/zander/internal/command"
@@ -14,9 +15,9 @@ type CLI struct {
 	Restart command.RestartServerCmd `cmd:"" help:"Restart a doom server"`
 	Attach  command.AttachCmd        `cmd:"" help:"Attach to a running doom server"`
 
-	Socket    string `flag:"" short:"s" type:"pathenv" default:"$XDG_RUNTIME_DIR/zander.sock" description:"Connects to a socket at the given path"`
-	LogLevel  string `flag:"" enum:"fatal,error,warn,debug,info,trace" default:"warn" help:"Only show the given log severity or higher. (valid values: ${enum})"`
-	LogFormat string `flag:"" enum:"text,json" default:"text" help:"Log output format. (valid values: ${enum})"`
+	Socket    string `flag:"" short:"s" type:"pathenv" env:"ZANDER_SOCKET" help:"Uses the given socket path for client/server communication. If no value is given, then it defaults to $XDG_RUNTIME_DIR/zander.sock"`
+	LogLevel  string `flag:"" enum:"fatal,error,warn,debug,info,trace" default:"warn" env:"ZANDER_LOG_LEVEL" help:"Only show the given log severity or higher. (valid values: ${enum})"`
+	LogFormat string `flag:"" enum:"text,json" default:"text" env:"ZANDER_LOG_FORMAT" help:"Log output format. (valid values: ${enum})"`
 }
 
 func main() {
@@ -25,8 +26,19 @@ func main() {
 
 	configureLogger(&cli)
 
+	socketPath := cli.Socket
+
+	if len(socketPath) == 0 {
+		p, err := xdg.RuntimeFile("zander.sock")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		socketPath = p
+	}
+
 	ctx.FatalIfErrorf(ctx.Run(command.CmdCtx{
-		Socket: cli.Socket,
+		Socket: socketPath,
 	}))
 }
 
